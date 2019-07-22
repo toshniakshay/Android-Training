5407a5a40b713051e01fd24ffd9075d7cf80862b
Akshay Toshniwal <toshniwal2@gmail.com>
2019-07-23 00:32:12 +0530

    Added Application class to project and Added simple User class.

---
+++ app/src/main/java/com/example/androidtraining/Enums/Gender.java
@@ -0,0 +1,7 @@
+package com.example.androidtraining.Enums;
+
+public enum Gender {
+    MALE,
+    FEMALE,
+    OTHER
+}
---
+++ app/src/main/java/com/example/androidtraining/Models/User.java
@@ -0,0 +1,71 @@
+package com.example.androidtraining.Models;
+
+/**
+ * Simple model class that will hold all the information with respect to user
+ */
+public class User {
+
+    private String firstName;
+    private String lastName;
+    private int gender;
+    private String country;
+    private String state;
+    private String emailAddress;
+    private String phoneNumber;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getGender() {
+        return gender;
+    }
+
+    public void setGender(int gender) {
+        this.gender = gender;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
